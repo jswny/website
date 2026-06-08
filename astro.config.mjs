02b37eedb3aeb4b13dcb2c1beb56252a53ed081d
Joe Sweeney <joe@j1.io>
2026-06-08 00:11:16 -0400

    Configure production domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://j1.io",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
