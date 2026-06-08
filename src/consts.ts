e44027f2f8cdb0a3c2c92ecba7b3c2f4104fdffc
Joe Sweeney <joe@j1.io>
2026-06-07 23:25:25 -0400

    Update homepage and remove about page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Joe Sweeney";
+export const SITE_DESCRIPTION =
+	"Software engineer interested in AI development, scaling systems, and real-time streaming.";
